441702215eb503cf09b5c0de35c1e41bf93931b1
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-04-03 11:29:09 +0200

    add download_url

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name = 'processlib',
     version = '0.0.1',
     url = 'https://github.com/RaphaelKimmig/processlib',
+    download_url = 'https://github.com/RaphaelKimmig/processlib/archive/0.0.1.tar.gz',
     author = 'Raphael Kimmig',
     author_email = 'raphael@ampad.de',
     description = 'A workflow library for python',
