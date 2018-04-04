86727473b206243e3d91d0af0ba23f8c173ce9fb
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-04-04 11:24:57 +0200

    use start activity to create process instance

--- processlib/serializers.py
+++ processlib/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
+from processlib.flow import get_flow
 from .models import Process
 
 
 class ProcessSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        flow_label = validated_data.pop('flow_label')
+        flow = get_flow(flow_label)
+        activity = flow.get_start_activity(**validated_data)
+        activity.start()
+        activity.finish()
+        return activity.process
+
     class Meta:
         model = Process
 
