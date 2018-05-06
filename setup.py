96d243888b42ba003f449100d5add962e5d3a1ec
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-05-06 15:21:29 +0200

    update version to 0.5

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='processlib',
-    version='0.0.1',
+    version='0.5',
     url='https://github.com/RaphaelKimmig/processlib',
-    download_url='https://github.com/RaphaelKimmig/processlib/archive/0.0.1.tar.gz',
+    download_url='https://github.com/RaphaelKimmig/processlib/archive/0.5.tar.gz',
     author='Raphael Kimmig',
     author_email='raphael@ampad.de',
     description='A workflow library for python',
     packages=find_packages(),
-    install_requires=['django >= 1.8', ],
+    install_requires=['django >= 1.11', ],
 )
