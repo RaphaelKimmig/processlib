722567a30ae2c77a210919d46e531c229f482a55
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-10-16 17:45:02 +0200

    add missing templates

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='processlib',
-    version='0.6.0',
+    version='0.6.1',
     url='https://github.com/RaphaelKimmig/processlib',
-    download_url='https://github.com/RaphaelKimmig/processlib/archive/0.6.0.tar.gz',
+    download_url='https://github.com/RaphaelKimmig/processlib/archive/0.6.1.tar.gz',
     author='Raphael Kimmig',
     author_email='raphael@ampad.de',
     description='A workflow library for python',
+    include_package_data=True,
     packages=find_packages(),
     install_requires=['django >= 1.10', ],
 )
