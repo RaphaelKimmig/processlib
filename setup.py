275a97dd8057bcb2c8dafc6b4132ac367601beb2
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-10-16 17:36:45 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='processlib',
-    version='0.5.5',
+    version='0.6.0',
     url='https://github.com/RaphaelKimmig/processlib',
-    download_url='https://github.com/RaphaelKimmig/processlib/archive/0.5.5.tar.gz',
+    download_url='https://github.com/RaphaelKimmig/processlib/archive/0.6.0.tar.gz',
     author='Raphael Kimmig',
     author_email='raphael@ampad.de',
     description='A workflow library for python',
