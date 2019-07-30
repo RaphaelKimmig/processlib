5893117a400034c275850b0d14344ea58e7f35d0
Raphael Kimmig <raphael.kimmig@ampad.de>
2019-07-30 09:25:18 +0200

    require _finish in POST to finish activities

--- processlib/views.py
+++ processlib/views.py
@@ -260,7 +260,8 @@ class ActivityMixin(CurrentAppMixin):
     """
 
     activity = None
-    _go_to_next = False
+    _finish_go_to_next = False
+    _finish = False
 
     def user_has_perm(self):
         return user_has_activity_perm(self.request.user, self.activity)
@@ -284,9 +285,11 @@ class ActivityMixin(CurrentAppMixin):
 
     def form_valid(self, *args, **kwargs):
         super(ActivityMixin, self).form_valid(*args, **kwargs)
-        user = self.request.user if self.request.user.is_authenticated else None
-        self.activity.finish(user=user)
-        self._go_to_next = "_go_to_next" in self.request.POST
+        if '_finish' in self.request.POST or '_finish_go_to_next' in self.request.POST:
+            user = self.request.user if self.request.user.is_authenticated else None
+            self.activity.finish(user=user)
+        self._finish_go_to_next = "_finish_go_to_next" in self.request.POST
+        self._finish = "_finish" in self.request.POST
         return HttpResponseRedirect(self.get_success_url())
 
     def dispatch(self, request, *args, **kwargs):
@@ -314,7 +317,7 @@ class ActivityMixin(CurrentAppMixin):
         return super(ActivityMixin, self).dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
-        if self._go_to_next:
+        if self._finish_go_to_next:
             for activity in get_current_activities_in_process(self.activity.process):
                 if activity.has_view() and user_has_activity_perm(
                     self.request.user, activity
@@ -327,11 +330,15 @@ class ActivityMixin(CurrentAppMixin):
                         },
                         current_app=self.get_current_app(),
                     )
-        return reverse(
-            "processlib:process-detail",
-            args=(self.activity.process.id,),
-            current_app=self.get_current_app(),
-        )
+        elif self._finish:
+            return reverse(
+                "processlib:process-detail",
+                args=(self.activity.process.id,),
+                current_app=self.get_current_app(),
+            )
+        else:
+            # redirect to current page if not finished
+            return self.request.get_full_path()
 
 
 class ProcessViewSet(viewsets.ModelViewSet):
