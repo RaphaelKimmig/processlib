994a3b7027e3cf94d6cb976fc54ed2edff13fe8a
Raphael Kimmig <raphael.kimmig@ampad.de>
2024-09-19 14:39:50 +0200

    release: 0.9.1

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='processlib',
-    version='0.9.0',
-    url='https://github.com/RaphaelKimmig/processlib',
-    download_url='https://github.com/RaphaelKimmig/processlib/archive/0.9.0.tar.gz',
-    author='Raphael Kimmig',
-    author_email='raphael@ampad.de',
-    description='A workflow library for python',
+    name="processlib",
+    version="0.9.1",
+    url="https://github.com/RaphaelKimmig/processlib",
+    download_url="https://github.com/RaphaelKimmig/processlib/archive/0.9.1.tar.gz",
+    author="Raphael Kimmig",
+    author_email="raphael@ampad.de",
+    description="A workflow library for python",
     include_package_data=True,
     packages=find_packages(),
-    install_requires=['django >= 3.2', ],
+    install_requires=[
+        "django >= 3.2",
+    ],
 )
